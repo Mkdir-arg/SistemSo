d5b4bf8f68b4f7a3102ee8d5a207d4f059e48c86
MKDIR <farinamatias00@gmail.com>
2026-02-20 18:07:04 -0300

    asd

--- legajos/context_processors.py
+++ legajos/context_processors.py
@@ -1,20 +1,25 @@
 from .models import EventoCritico, AlertaEventoCritico
+from django.utils.asyncio import async_unsafe
 
 
+@async_unsafe
 def alertas_eventos_criticos(request):
     """Context processor para mostrar alertas de eventos críticos al responsable"""
     
     if not request.user.is_authenticated:
         return {}
     
-    # Obtener eventos críticos de legajos donde el usuario es responsable
-    # y que no han sido vistos por el responsable
-    eventos_pendientes = EventoCritico.objects.filter(
-        legajo__responsable=request.user
-    ).exclude(
-        alertas_vistas__responsable=request.user
-    ).select_related('legajo__ciudadano').order_by('-creado')[:5]  # Máximo 5 alertas
-    
-    return {
-        'eventos_criticos_pendientes': eventos_pendientes
-    }
+    try:
+        # Obtener eventos críticos de legajos donde el usuario es responsable
+        # y que no han sido vistos por el responsable
+        eventos_pendientes = EventoCritico.objects.filter(
+            legajo__responsable=request.user
+        ).exclude(
+            alertas_vistas__responsable=request.user
+        ).select_related('legajo__ciudadano').order_by('-creado')[:5]  # Máximo 5 alertas
+        
+        return {
+            'eventos_criticos_pendientes': eventos_pendientes
+        }
+    except Exception:
+        return {'eventos_criticos_pendientes': []}
