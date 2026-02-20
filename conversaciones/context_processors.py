c85ecbbee99437bfe624d3eb3fd262fd7eb03d74
MKDIR <farinamatias00@gmail.com>
2026-02-20 17:27:43 -0300

    Hotfix

--- conversaciones/context_processors.py
+++ conversaciones/context_processors.py
@@ -1,7 +1,14 @@
+from asgiref.sync import sync_to_async
+from django.utils.asyncio import async_unsafe
+
+@async_unsafe
 def user_groups(request):
     """Context processor para pasar los grupos del usuario al template"""
     if request.user.is_authenticated:
-        groups = list(request.user.groups.values_list('name', flat=True))
+        try:
+            groups = list(request.user.groups.values_list('name', flat=True))
+        except Exception:
+            groups = []
         return {
             'user_groups_list': groups,
             'user_groups_json': str(groups).replace("'", '"')
