7d66259d235dee538bf5b2da6ae20235d71b3386
juanikitro <jpkitro@gmail.com>
2026-03-13 10:22:39 +0800

    refactor(dx): slice 25 legajos forms cleanup

---
+++ legajos/tests/test_forms_fachada.py
@@ -0,0 +1,45 @@
+from django.test import SimpleTestCase
+
+from legajos import forms
+from legajos.forms_ciudadanos import (
+    AdmisionLegajoForm,
+    BuscarCiudadanoForm,
+    CiudadanoConfirmarForm,
+    CiudadanoManualForm,
+    CiudadanoUpdateForm,
+    ConsentimientoForm,
+    ConsultaRenaperForm,
+)
+from legajos.forms_clinico import (
+    DerivacionForm,
+    EvaluacionInicialForm,
+    EventoCriticoForm,
+    LegajoCerrarForm,
+    LegajoReabrirForm,
+    PlanIntervencionForm,
+    SeguimientoForm,
+)
+from legajos.forms_operativa import InscribirActividadForm
+
+
+class LegajosFormsFachadaTests(SimpleTestCase):
+    def test_expone_forms_de_ciudadanos(self):
+        self.assertIs(forms.ConsultaRenaperForm, ConsultaRenaperForm)
+        self.assertIs(forms.CiudadanoManualForm, CiudadanoManualForm)
+        self.assertIs(forms.CiudadanoConfirmarForm, CiudadanoConfirmarForm)
+        self.assertIs(forms.CiudadanoUpdateForm, CiudadanoUpdateForm)
+        self.assertIs(forms.BuscarCiudadanoForm, BuscarCiudadanoForm)
+        self.assertIs(forms.AdmisionLegajoForm, AdmisionLegajoForm)
+        self.assertIs(forms.ConsentimientoForm, ConsentimientoForm)
+
+    def test_expone_forms_clinicos(self):
+        self.assertIs(forms.EvaluacionInicialForm, EvaluacionInicialForm)
+        self.assertIs(forms.PlanIntervencionForm, PlanIntervencionForm)
+        self.assertIs(forms.SeguimientoForm, SeguimientoForm)
+        self.assertIs(forms.DerivacionForm, DerivacionForm)
+        self.assertIs(forms.EventoCriticoForm, EventoCriticoForm)
+        self.assertIs(forms.LegajoCerrarForm, LegajoCerrarForm)
+        self.assertIs(forms.LegajoReabrirForm, LegajoReabrirForm)
+
+    def test_expone_forms_operativos(self):
+        self.assertIs(forms.InscribirActividadForm, InscribirActividadForm)
